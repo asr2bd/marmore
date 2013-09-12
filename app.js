bb991afb6ca46e493afd1180644611334f7cfc37
Paul Ammann <meanmrclean@gmail.com>
2013-09-12 15:59:28 -0400

    new about at /about.html

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res){
 	res.render("index");
 });
+app.get('/about.html',function(req,res){
+	res.render("about");
+});
 
 
 var port = process.env.PORT || 3000;
